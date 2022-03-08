1c611ef3af67ea20bbe447f6e849f377abfbb90b
Nestor De j. Lopez Uscanga <nestorj.lopez@gmail.com>
2022-03-08 13:20:42 -0600

    aplicacion inicial

---
+++ sociomobile/android/app/src/main/java/com/qido/socioApp/MainActivity.java
@@ -0,0 +1,6 @@
+package com.qido.socioApp;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ sociomobile/lib/services/authentication_service.dart
@@ -0,0 +1,49 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+
+class AuthenticationService {
+
+  final FirebaseAuth _firebaseAuth;
+
+  AuthenticationService(this._firebaseAuth);
+
+  Stream<User?> get authStateChanges => _firebaseAuth.authStateChanges();
+
+  Future<String?> signIn({required String email, required String password}) async {
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(email: email, password: password);
+      return "Signed in";
+    } on FirebaseAuthException catch(e) {
+      return e.message;
+    }
+  }
+
+  // 4
+  Future<String?> signUp({required String email, required String password}) async {
+    try {
+      await _firebaseAuth.createUserWithEmailAndPassword(email: email, password: password);
+      return "Signed up";
+    } on FirebaseAuthException catch(e) {
+      return e.message;
+    }
+  }
+
+  // 5
+  Future<String?> signOut() async {
+    try {
+      await _firebaseAuth.signOut();
+      return "Signed out";
+    } on FirebaseAuthException catch(e) {
+      return e.message;
+    }
+  }
+
+  User? getUser() {
+    try {
+      return _firebaseAuth.currentUser;
+    } on FirebaseAuthException {
+      return null;
+    }
+  }
+
+}
