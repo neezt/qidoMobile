91f9e8f2b8a46b7839e55bc8701651f84fe7c13b
Nestor De j. Lopez Uscanga <nestorj.lopez@gmail.com>
2022-05-29 21:37:23 -0500

    conexión de login con usuarios en firebase

---
+++ sociomobile/lib/utils/fire_auth.dart
@@ -0,0 +1,89 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+class FireAuth {
+  static String token="";
+  // For registering a new user
+  static Future<User> registerUsingEmailPassword({
+    String name,
+    String email,
+    String password,
+  }) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User user;
+
+    try {
+      UserCredential userCredential = await auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      user = userCredential.user;
+      await user.updateProfile(displayName: name);
+      await user.reload();
+      user = auth.currentUser;
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        print('The password provided is too weak.');
+      } else if (e.code == 'email-already-in-use') {
+        print('The account already exists for that email.');
+      }
+    } catch (e) {
+      print(e);
+    }
+
+    return user;
+  }
+
+  // For signing in an user (have already registered)
+  static Future<User> signInUsingEmailPassword({
+    String email,
+    String password,
+  }) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+    User user;
+
+    try {
+      UserCredential userCredential = await auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      user = userCredential.user;
+      fetchData(email, password);
+
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'user-not-found') {
+        print('No user found for that email.');
+      } else if (e.code == 'wrong-password') {
+        print('Wrong password provided.');
+      }
+    }
+
+    return user;
+  }
+
+  static Future<User> refreshUser(User user) async {
+    FirebaseAuth auth = FirebaseAuth.instance;
+
+    await user.reload();
+    User refreshedUser = auth.currentUser;
+
+    return refreshedUser;
+  }
+
+  static Future<String> fetchData(String email,String password) async {
+    http.Response response;
+    response = await http.post(Uri.parse('https://otconsultingback.comercioincoterms.com/login/iniciar'),
+        body: jsonEncode(<String, String>{
+          'email': email,"password":password
+        }));
+    if (response.statusCode==200) {
+      Map mapResponse =  json.decode(response.body);
+      token= mapResponse['refreshToken'];
+      print("otro $token");
+    }
+    return "";
+  }
+
+}
